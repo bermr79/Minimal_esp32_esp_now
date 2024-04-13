1132ba41a98cf5d21ffc007581b12ec74de64f61
Berna79 <bernardomr79@gmail.com>
2024-04-13 21:49:43 +0200

    renew the proyect -> Platformio

---
+++ src/sacarMac.ino
@@ -0,0 +1,19 @@
+#include <Arduino.h>
+#include "WiFi.h"
+
+void setup() {
+  Serial.begin(9600);
+  WiFi.mode(WIFI_STA);
+  Serial.println("Mac reader_>");
+}
+
+void loop() {
+  Mimac();
+  delay(5000);
+}
+
+void Mimac() {
+  Serial.println("MiMAC:");
+  Serial.println(WiFi.macAddress());
+  delay(5000);
+}
